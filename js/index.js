27ffcf88fec4b4645cbdf6ce4a08302f358368b1
Raccoonvoor <thiagovieiraracoon@gmail.com>
2024-08-29 10:36:42 -0300

    att

--- js/index.js
+++ js/index.js
@@ -2,7 +2,23 @@ const diaSemana = document.getElementById("dia-semana");
 const diaMesAno= document.getElementById("dia-mes-ano");
 const horaMinSeg = document.getElementById("hora-min-seg");
 
-const date = new
 
-console.log(date.getDay());
-console.log(date.getMonth());
+// Todo conjunto numérico (exceto ano ) deve ter 2 digitos (adicionar 0 se for menor q 10)
+// Retornar dia da semana por extenso(em pt-br)
+
+function diaSemana(){
+    // retornar dia da semana
+}
+
+function dataCompleta() {
+    const date = new Date();
+    return date.getDate() + "/" + date.getMonth() + "/" + date.getFullYear();
+}
+
+function horaCompleta() {
+    const date = new date();
+    return date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+}
+
+diaMesAno.textContent = dataCompleta();
+horaMinSeg.textContent = horaCompleta();
