3d1404c7ead245fa750b958289e48e5174e1e36b
Raccoonvoor <thiagovieiraracoon@gmail.com>
2024-08-29 09:47:05 -0300

    teste

---
+++ js/index.js
@@ -0,0 +1,7 @@
+const diaSemana = document.getElementById("dia-semana");
+const diaMesAno= document.getElementById("dia-mes-ano");
+const horaMinSeg = document.getElementById("hora-min-seg");
+
+diaSemana.textContent ="wdadwa";
+
+console.log(diaSemana.textContent);
